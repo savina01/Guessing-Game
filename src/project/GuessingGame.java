cd88bb772e147ddba52041605483fe84cca4f395
Savina Valchanova <75179050+savina01@users.noreply.github.com>
2022-02-05 21:09:27 +0200

    Add files via upload

---
+++ src/project/GuessingGame.java
@@ -0,0 +1,45 @@
+package project;
+
+import javax.swing.*;
+
+public class GuessingGame {
+    public static void main(String[] args) {
+        int computerNumber = (int) (Math.random()*100 + 1);
+        int userAnswer = 0;
+        System.out.println("The correct guess would be " + computerNumber);
+        int count = 1;
+
+        while (userAnswer != computerNumber)
+        {
+
+            String response = JOptionPane.showInputDialog(null,
+                    "Enter a guess between 1 and 100", "Guessing Game", 3);
+            if(!response.isEmpty()) {
+                userAnswer = Integer.parseInt(response);
+                JOptionPane.showMessageDialog(null, "" + determineGuess(userAnswer, computerNumber, count));
+                count++;
+            }
+            else
+                JOptionPane.showMessageDialog(null,"Please enter a number" ,"Warming",2);
+
+        }
+    }
+
+    public static String determineGuess(int userAnswer, int computerNumber, int count){
+        if (userAnswer <=0 || userAnswer > 100) {
+            return "Your guess is invalid";
+        }
+        else if (userAnswer == computerNumber ){
+            return "Correct!\nTotal Guesses: " + count;
+        }
+        else if (userAnswer > computerNumber) {
+            return "Your guess is too high, try again.\nTry Number: " + count;
+        }
+        else if (userAnswer < computerNumber) {
+            return "Your guess is too low, try again.\nTry Number: " + count;
+        }
+        else {
+            return "Your guess is incorrect\nTry Number: " + count;
+        }
+    }
+}
